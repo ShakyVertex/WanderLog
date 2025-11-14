b7f13601daaa3fc2cddd918d33d8ffcd48a367ef
Jen Gordon <86987150+Jericgordon@users.noreply.github.com>
2025-11-14 12:10:48 -0700

    Strict mode shouldn't be enable for deployment

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -7,10 +7,10 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-root.render(
-  <React.StrictMode>
+root.render( 
+  <React>
     <App />
-  </React.StrictMode>
+  </React>
 );
 
 // If you want to start measuring performance in your app, pass a function
